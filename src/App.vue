231112837d16f96721578a6f17a9d10efa4d02fc
mmq <maomeiqing1@flkj.ai>
2023-11-12 19:26:52 +0800

    chore: 初始化项目并进行初步配置

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>我是APP</h1>
+    <el-button type="primary" :icon="Edit">Primary</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 引入图标组件
+import { Edit } from '@element-plus/icons-vue'
+</script>
+
+<style scoped></style>
